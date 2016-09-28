a3125c57324ab28c0f98836f529ae87b3f617727
aridoza <aridoza85@gmail.com>
2016-09-27 23:31:55 -0400

    frontend troubleshoot

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { ReactDOM, render } from "react-dom";
 
-import App from './containers/App';
+
 
 render(
-  <App />,
+  <h3>Hello World!</h3>
   document.getElementById('parkme-app')
 );
